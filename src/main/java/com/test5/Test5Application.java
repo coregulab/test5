422883d79a3ad1c8839368075849c4d85e55da3e
lalkumar-dev <javainfo.lk@gmail.com>
2026-01-20 12:38:07 +0530

    first print

--- src/main/java/com/test5/Test5Application.java
+++ src/main/java/com/test5/Test5Application.java
@@ -8,6 +8,8 @@ public class Test5Application {
 
 	public static void main(String[] args) {
 		int x=100;
+		int y=200;
+		System.out.println(x);
 		SpringApplication.run(Test5Application.class, args);
 	}
 
